7466f71de11a26085e473157b16672c8d022c710
jpaomat@gmail.com <jpaomat@gmail.com>
2020-01-04 23:55:41 -0500

    router Module

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+
+  constructor(private routerModule: Router) { }
+
+  ngOnInit() {
+  };
+  goToPosts(){
+    this.routerModule.navigate(['/posts']);
+  }
+
+}
