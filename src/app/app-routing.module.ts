185c37b27e1ac82cd9b2ce25abaaf4c360c08e0f
jpaomat@gmail.com <jpaomat@gmail.com>
2020-01-05 20:26:37 -0500

    pathMatch-wildcard(**)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ const routes: Routes = [
   {path:'users', component:UsersComponent},
   {path:'posts', component:PostsComponent},
   {path:'users/:id/posts', component:PostsComponent},
-  {path:'', component:UsersComponent},
+  {path:'', redirectTo:'/users', pathMatch:'full'},//con el patchMatch full el path debe coindicir tal cual pero si se usa prefix desde que este de primeras la parte del path lo redirige exitosamente
+ // {path:'**', component: NotFoundComponent} //ESTE SE PONE CUANDO LA URL ESTA MAL ENTONCES SE REDIRIGE A UN COMPONENTE NOTFOUND Q SE DEBE CREAR 
 ];
 
 @NgModule({
